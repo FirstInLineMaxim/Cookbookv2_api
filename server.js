d1c6c5ca9e6499f34635958a9ea0e746131df42b
Tiefengnom <111353598+Tiefengnom@users.noreply.github.com>
2022-11-21 16:23:20 +0100

    s

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path =require('path');
 const PORT = 3000
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+/*app.get('/', (req, res) => {
     res.send('Hello World')
+});*/
+
+app.get('/', (req,res) => {
+fs.readFile('./data.json',(err,data) => {
+    if (err) throw err;
+    let pcontent = JSON.parse(data);
+    res.send(pcontent)})
+
 });
+
+
+
+/*app.get('/json', (req,res) => {
+    const data = require('./data.json')
+    console.log(data);
+    
+    });*/
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log('App listening on port http://localhost:3000 !');
