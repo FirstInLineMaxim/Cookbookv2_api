1a2ac3e8ca6b047cf7a598ae1defa41d43909ebc
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-22 14:00:43 +0100

    cloudinary

--- cloudinary.js
+++ cloudinary.js
@@ -5,25 +5,25 @@ cloudinary.config({
     api_secret: 'yTTsa_MxsuOp1m75OCZs-1VbdkY' 
   });
 
-  const uploadImage = async (imagePath) => {
+//   const uploadImage = async (imagePath) => {
 
-    // Use the uploaded file's name as the asset's public ID and 
-    // allow overwriting the asset with new versions
-    const options = {
-      use_filename: true,
-      unique_filename: false,
-      overwrite: true,
-    };
+//     // Use the uploaded file's name as the asset's public ID and 
+//     // allow overwriting the asset with new versions
+//     const options = {
+//       use_filename: true,
+//       unique_filename: false,
+//       overwrite: true,
+//     };
 
-    try {
-      // Upload the image
-      const result = await cloudinary.uploader.upload(imagePath, options);
-      console.log(result);
-      return result.public_id;
-    } catch (error) {
-      console.error(error);
-    }
-};
+//     try {
+//       // Upload the image
+//       const result = await cloudinary.uploader.upload(imagePath, options);
+//       console.log(result);
+//       return result.public_id;
+//     } catch (error) {
+//       console.error(error);
+//     }
+// };
 
     //ImageUpload
     // cloudinary.uploader.upload("blob:http://localhost:3000/921e66ab-d638-4ea5-aeb7-5cf182b1f0a6", (error, result)=>{
