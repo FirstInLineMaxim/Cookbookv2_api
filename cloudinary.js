170a34d1805f8ead22e9003cd1f4f4f25c95bcba
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-22 14:00:25 +0100

    uuid

---
+++ cloudinary.js
@@ -0,0 +1,33 @@
+const cloudinary = require('cloudinary').v2
+cloudinary.config({ 
+    cloud_name: 'dwwm12zrf', 
+    api_key: '744777837789432', 
+    api_secret: 'yTTsa_MxsuOp1m75OCZs-1VbdkY' 
+  });
+
+  const uploadImage = async (imagePath) => {
+
+    // Use the uploaded file's name as the asset's public ID and 
+    // allow overwriting the asset with new versions
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
+
+    try {
+      // Upload the image
+      const result = await cloudinary.uploader.upload(imagePath, options);
+      console.log(result);
+      return result.public_id;
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+    //ImageUpload
+    // cloudinary.uploader.upload("blob:http://localhost:3000/921e66ab-d638-4ea5-aeb7-5cf182b1f0a6", (error, result)=>{
+    //     console.log(result, error);
+    //   });
+
+module.exports = cloudinary;
--- server.js
+++ server.js
@@ -1,59 +1,63 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
-const {v4: uuid} = require('uuid')
-const path = require('path')
-const { fstat } = require('fs')
-const PORT = 3000
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const path = require("path");
+const PORT = 3000;
+const cloudinary = require("./cloudinary");
 
-//Parse for the request body 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+//Parse for the request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // The From with the post request to push inot our .json file same as the /content path
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/test.html'));
-  });
-  
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/test.html"));
+});
 
-// generates a uuid
-// app.post('/uuid', (req, res) => {
-//     const id = uuid()
-//     const content = req.body
-//     if(!content){
-//         console.log(req.body)
-//         return res.sendStatus(400)
-//     }
-//     console.log(req.body)
-
-//     res.status(201).json({id:id})
-// });
-
-//Basic displaying of json 
-app.get('/entries', (req, res) => {
-    const data = require('./data/data.json')
-    res.json(data)
-    
+//Basic displaying of json
+app.get("/entries", (req, res) => {
+  const data = require("./data/data.json");
+  res.json(data);
 });
 
 // The From with the post request to push inot our .json file
-app.get('/content', (req, res) => {
-    res.sendFile(path.join(__dirname, '/test.html'))
+app.get("/content", (req, res) => {
+  res.sendFile(path.join(__dirname, "/test.html"));
 });
+
 // The Post request who reads and writes to data.json
-app.post('/content', (req, res) => {
-    //writes files after another into the file. needs {flags :'a'} to work  https://nodejs.org/api/fs.html#file-system-flags
-    const data = fs.readFileSync("./data/data.json");
-    const myObject = JSON.parse(data);
-    const newData = req.body
-    myObject.push(newData)
-    const newData2 = JSON.stringify(myObject);
-    fs.writeFile("./data/data.json", newData2, (err) => {
+app.post("/content", (req, res) => {
+  //writes files after another into the file. needs {flags :'a'} to work  https://nodejs.org/api/fs.html#file-system-flags
+  const data = fs.readFileSync("./data/data.json");
+  const myObject = JSON.parse(data);
+  const { ...form } = req.body;
+  //Just the model for the json data
+  const newData = {
+    id: uuid(),
+    title: form.title,
+    description: form.description,
+    ingredients: form.ingredients,
+    instructions: form.instructions,
+    mainImage: form.mainImage,
+    ...form,
+  };
+  // const newData = req.bodyv
+  myObject.push(newData);
+  const newData2 = JSON.stringify(myObject);
+  fs.writeFile("./data/data.json", newData2, (err) => {
     // Error checking
-     if (err) throw err;
+    if (err) throw err;
     console.log("New data added");
-    });
+  });
+  res.status(201);
+      cloudinary.uploader.upload("blob:http://localhost:3000/921e66ab-d638-4ea5-aeb7-5cf182b1f0a6", (error, result)=>{
+        console.log(result, error);
+      });
 });
 
 app.listen(PORT, () => {
-    console.log('App listening on port http://localhost:3000 !');
+  console.log("App listening on port http://localhost:3000 !");
 });
+
+/////////////////////
+//Getting the Image//
