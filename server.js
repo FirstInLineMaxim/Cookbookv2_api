29916a62716ae058c6c6bb93f008589ba4ea4959
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-24 18:08:04 +0100

    Co-authored-by: frankasamoah <frankasamoah@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const path = require("path");
 const uploadRouter = require("./Routes/uploadRouter");
@@ -7,6 +8,7 @@ const pool = require("./configs/sqlconfig");
 const PORT = 3000;
 
 //Parse for the request body
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // The From with the post request to push inot our .json file same as the /content path
